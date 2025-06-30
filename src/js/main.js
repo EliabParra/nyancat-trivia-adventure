60750faf91c031e90618072eafd6055b9cd9c273
Eliab Parra <eliabparra@gmail.com>
2025-06-30 13:57:25 -0400

    Refactor main.js to remove unnecesary animation

--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,4 @@ import SetupScreenComponent from "./SetupScreenComponent/setup.js"
 import AudioComponent from "./AudioComponent/audio.js"
 import PixelStarsComponent from "./PixelStarsComponent/stars.js"
 
-const setupScreen = new SetupScreenComponent()
-
-document.addEventListener("click", (e) => {
-    if (e.target.matches("button, .category-option")) {
-        e.target.style.transform = "scale(0.95)"
-        setTimeout(() => {
-            e.target.style.transform = ""
-        }, 100)
-    }
-})
+new SetupScreenComponent()
